67ee4cba34108b318ab08c37420552a03e75869c
Victoria Teixeira <victoriafteixeira@hotmail.com>
2023-05-03 09:51:57 -0300

    feat & refactor: cria as models AbstractODM e MotorcycleODM e refatora a model CarODM

---
+++ src/Models/AbstractODM.ts
@@ -0,0 +1,43 @@
+import { Model, Schema, models,
+  model, isValidObjectId, UpdateQuery } from 'mongoose';
+import BadRequestError from '../Errors/BadRequestError';
+import NotFoundError from '../Errors/NotFoundError';
+
+abstract class AbstractODM<T> {
+  protected model: Model<T>;
+  private schema: Schema;
+  protected modelName: string;
+
+  constructor(schema: Schema, modelName: string) {
+    this.schema = schema;
+    this.modelName = modelName;
+    this.model = models[this.modelName] || model(modelName, this.schema);
+  }
+
+  public async create(obj: T): Promise<T> {
+    return this.model.create({ ...obj });
+  }
+  public async getAll(): Promise<T[]> {
+    return this.model.find();
+  }
+
+  public async getById(id: string): Promise <T | null> {
+    if (!isValidObjectId(id)) throw new BadRequestError('Invalid mongo id');
+    return this.model.findById(id);
+  }
+
+  public async update(id: string, obj: Partial<T>):
+  Promise<T | null> {
+    const isId = await this.getById(id);
+    if (isId === null) throw new NotFoundError('Car not found');
+    // if (!isValidObjectId(id)) throw new BadRequestError('Invalid Mongo id');
+    
+    return this.model.findByIdAndUpdate(
+      { _id: id },
+      { ...obj } as UpdateQuery<T>,
+      { new: true },
+    );   
+  }
+}
+
+export default AbstractODM;
--- src/Models/CarODM.ts
+++ src/Models/CarODM.ts
@@ -1,16 +1,12 @@
-import { Model, Schema, model, models, isValidObjectId, UpdateQuery } from 'mongoose';
+import { Schema } from 'mongoose';
 
 import ICar from '../Interfaces/ICar';
 
-import BadRequestError from '../Errors/BadRequestError';
-import NotFoundError from '../Errors/NotFoundError';
+import AbstractODM from './AbstractODM';
 
-class CarODM {
-  private schema: Schema;
-  private model: Model<ICar>;
-
+class CarODM extends AbstractODM<ICar> {
   constructor() {
-    this.schema = new Schema<ICar>({
+    const schema = new Schema<ICar>({
       model: { type: String, required: true },
       year: { type: Number, required: true },
       color: { type: String, required: true },
@@ -19,33 +15,33 @@ class CarODM {
       doorsQty: { type: Number, required: true },
       seatsQty: { type: Number, required: true },
     });
-    this.model = models.Car || model('Car', this.schema);
+    super(schema, 'Car');
   }
 
-  public async create(car: ICar): Promise<ICar> {
-    return this.model.create({ ...car });
-  }
+  // public async create(car: ICar): Promise<ICar> {
+  //   return this.model.create({ ...car });
+  // }
 
-  public async getAll(): Promise<ICar[]> {
-    return this.model.find();
-  }
+  // public async getAll(): Promise<ICar[]> {
+  //   return this.model.find();
+  // }
 
-  public async getById(id: string): Promise <ICar | null> {
-    if (!isValidObjectId(id)) throw new BadRequestError('Invalid mongo id');
-    const car = this.model.findById(id);
-    // if (!car) throw new NotFoundError('Car not found');
-    return car;
-  }
+  // public async getById(id: string): Promise <ICar | null> {
+  //   if (!isValidObjectId(id)) throw new BadRequestError('Invalid mongo id');
+  //   const car = this.model.findById(id);
+  //   // if (!car) throw new NotFoundError('Car not found');
+  //   return car;
+  // }
 
-  public async update(id: string, car: ICar): Promise<ICar | null> {
-    const isId = await this.getById(id);
-    if (isId === null) throw new NotFoundError('Car not found');
-    const updatedCar = this.model.findByIdAndUpdate(
-      { _id: id },
-      { ...car } as UpdateQuery <ICar>,
-      { new: true },
-    );
-    return updatedCar;
-  }
+  // public async update(id: string, car: ICar): Promise<ICar | null> {
+  //   const isId = await this.getById(id);
+  //   if (isId === null) throw new NotFoundError('Car not found');
+  //   const updatedCar = this.model.findByIdAndUpdate(
+  //     { _id: id },
+  //     { ...car } as UpdateQuery <ICar>,
+  //     { new: true },
+  //   );
+  //   return updatedCar;
+  // }
 }
 export default CarODM;
